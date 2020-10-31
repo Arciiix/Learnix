b2d91cff18bd5197784c001395289d261bd03153
Arciiix <artur.nowak100@gmail.com>
2020-10-31 23:24:48 +0100

    Start doing home page

--- learnix/src/Components/Home.js
+++ learnix/src/Components/Home.js
@@ -1,8 +1,95 @@
 import React from "react";
 
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  InputBase,
+  Typography,
+  Drawer,
+  Button,
+} from "@material-ui/core";
+import { Search, Menu, AccountCircle, ExitToApp } from "@material-ui/icons/";
+
+import "./css/Home.css";
+
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: false,
+      title: "Learnix",
+    };
+  }
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize.bind(this));
+    this.handleResize();
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize.bind(this));
+  }
+  handleResize() {
+    let vw = Math.max(
+      document.documentElement.clientWidth || 0,
+      window.innerWidth || 0
+    );
+    console.log(vw);
+    if (vw < 768) {
+      if (this.state.title !== "L") {
+        this.setState({ title: "L" });
+      }
+    } else if (this.state.title !== "Learnix") {
+      this.setState({ title: "Learnix" });
+    }
+  }
   render() {
-    return <h1>ilovegod</h1>;
+    return (
+      <div className="home">
+        <Drawer
+          anchor="left"
+          open={this.state.isMenuOpen}
+          onBackdropClick={this.toogleMenu.bind(this)}
+          onEscapeKeyDown={this.toogleMenu.bind(this)}
+        >
+          <div className="drawerContent">
+            <div className="userLogOut">
+              <AccountCircle fontSize="large" className="drawerAccount" />
+              <Typography className="barText" className="drawerAccount">
+                NAZWA
+              </Typography>
+              <div className="logOutBtn">
+                <Button fullWidth size="large">
+                  Wyloguj
+                </Button>
+              </div>
+            </div>
+          </div>
+        </Drawer>
+        <AppBar position="static" className="appBar">
+          <Toolbar className="appBarContent">
+            <div className="barPart">
+              <IconButton edge="start" color="inherit">
+                <Menu fontSize="large" onClick={this.toogleMenu.bind(this)} />
+              </IconButton>
+              <Typography className="barText">{this.state.title}</Typography>
+            </div>
+            <div className="barPart">
+              <InputBase
+                placeholder="Szukaj…"
+                className="searchInput"
+                inputProps={{ "aria-label": "Szukaj" }}
+              />
+              <IconButton edge="start" color="inherit">
+                <Search fontSize="large" />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+  toogleMenu() {
+    this.setState({ isMenuOpen: !this.state.isMenuOpen });
   }
 }
 
