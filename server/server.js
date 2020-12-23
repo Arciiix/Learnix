21ccc5b76a13ea22382a6da6b7747df661965e66
Arciiix <artur.nowak100@gmail.com>
2020-12-23 19:57:31 +0100

    Make the log function

--- server/server.js
+++ server/server.js
@@ -11,15 +11,26 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+function log(content, isError) {
+  if (isError) {
+    console.error(
+      '\x1b[31m%s\x1b[0m"',
+      `[ERROR] [${dateToTime(new Date())}] ${content}` //The first part of log makes the font red (copied from https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color - ANSI escape code)
+    );
+  } else {
+    console.log(`[LOG] [${dateToTime(new Date())}] ${content}`);
+  }
+
+  return content;
+}
+
 function dateToTime(date) {
   if (!(date instanceof Date)) {
-    return "ERR while parsing date";
+    return log("ERR while parsing date", true);
   }
   return date.toLocaleDateString() + " " + date.toLocaleTimeString();
 }
 
 app.listen(PORT, () => {
-  console.log(
-    `[${dateToTime(new Date())}] App has started on the port ${PORT}`
-  );
+  log(`App has started on the port ${PORT}`);
 });
